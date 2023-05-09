48f03d991db241ffc69e353b0c3ef8dc72e01fa5
mikel-m <56072711+mikel-m@users.noreply.github.com>
2023-05-09 12:05:08 +0200

    Update app.py to rock paper scissors game

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+"""
 import socket
 from flask import Flask,jsonify
 app_port = 5000
@@ -16,3 +17,42 @@ def home():
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int(app_port), debug=True)
 	
+"""
+# Rock, Paper and Scissors Game with Python
+import random
+choices = ["Rock", "Paper", "Scissors"]
+computer = random.choice(choices)
+player = False
+cpu_score = 0
+player_score = 0
+while True:
+    player = input("Rock, Paper or  Scissors?").capitalize()
+    ## Conditions of Rock,Paper and Scissors
+    if player == computer:
+        print("Tie!")
+    elif player == "Rock":
+        if computer == "Paper":
+            print("You lose!", computer, "covers", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "smashes", computer)
+            player_score+=1
+    elif player == "Paper":
+        if computer == "Scissors":
+            print("You lose!", computer, "cut", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "covers", computer)
+            player_score+=1
+    elif player == "Scissors":
+        if computer == "Rock":
+            print("You lose...", computer, "smashes", player)
+            cpu_score+=1
+        else:
+            print("You win!", player, "cut", computer)
+            player_score+=1
+    elif player=='End':
+        print("Final Scores:")
+        print(f"CPU:{cpu_score}")
+        print(f"Player:{player_score}")
+        break
